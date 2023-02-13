7aa751e94f56bd010ea9c1f79606c384dcd145b0
Veer Bahadur Tiwari <veeru.ramjas@gmail.com>
2023-02-13 19:19:10 +0530

    basic Button and Label for score added on VC

--- TennisKata/View/ViewController.swift
+++ TennisKata/View/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel : UILabel!
+    
+    //MARK: - ViewModel
+    private var viewModel : MainHomeViewModel! = nil
+    private var bindings = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewModel = MainHomeViewModel(firstPlayerName: "Player 1", secondPlayerName: "Player 2")
+        viewModel.$scoreMessage.receive(on: DispatchQueue.main).sink { [weak self] message in
+            self?.scoreLabel.text = message
+        }.store(in: &bindings)
+        
+        viewModel.$gameState.receive(on: DispatchQueue.main).sink { [weak self] state in
+            if state == .ended {
+                self?.showWinnerAlert()
+            }
+        }.store(in: &bindings)
+        
+        viewModel.startGame()
     }
 
+    @IBAction func firstPlayerScores(_ sender: Any) {
+        if !self.isGameEnded() {
+            viewModel.firstPlayerScores()
+        }
+    }
+    
+    @IBAction func secondPlayerScorers(_ sender: Any) {
+        if !self.isGameEnded() {
+            viewModel.secondPlayerScores()
+        }
+    }
+    
+    private func isGameEnded() -> Bool {
+        if viewModel.gameState == .ended {
+            let alert =  UIAlertController(title: "Game Ended", message: "Do you want to start again?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Start again", style: .default, handler: { _ in
+                self.startAgain()
+            }))
+            self.present(alert, animated: true, completion: nil)
+            return true
+        }
+        return false
+    }
+    
+    private func startAgain() {
+        viewModel.startGame()
+    }
+    
+    private func showWinnerAlert() {
+        let alert =  UIAlertController(title: "Alert", message: viewModel.scoreMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .destructive, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
 
